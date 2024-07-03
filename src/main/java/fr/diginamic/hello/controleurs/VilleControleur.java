aafdc3c26477cf998d297c50cd7880c50d25569a
Noémie Letenneur <n.letenneur@gmail.com>
2024-07-03 16:52:58 +0200

    TP5

--- src/main/java/fr/diginamic/hello/controleurs/VilleControleur.java
+++ src/main/java/fr/diginamic/hello/controleurs/VilleControleur.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,35 +19,71 @@ import fr.diginamic.hello.entities.Ville;
 @RequestMapping("/villes")
 public class VilleControleur {
 
-	ArrayList<Ville> villes = new ArrayList<>();
+	ArrayList<Ville> villes = creerVilles();
 
-	@GetMapping
-	public ArrayList<Ville> CreerVilles() {
+	public ArrayList<Ville> creerVilles() {
+		ArrayList<Ville> villes = new ArrayList<>();
 		// Création de la liste de villes
-		Ville v01 = new Ville("Nice", 343000);
-		Ville v02 = new Ville("Carcassone", 47800);
-		Ville v03 = new Ville("Narbonne", 53400);
-		Ville v04 = new Ville("Lyon", 484000);
-		Ville v05 = new Ville("Foix", 9700);
-		Ville v06 = new Ville("Pau", 77200);
-		Ville v07 = new Ville("Marseille", 850700);
-		Ville v08 = new Ville("Tarbes", 40600);
-
+		Ville v01 = new Ville(1, "Nice", 343000);
+		Ville v02 = new Ville(2, "Carcassone", 47800);
+		Ville v03 = new Ville(3, "Narbonne", 53400);
+		Ville v04 = new Ville(4, "Lyon", 484000);
+		Ville v05 = new Ville(5, "Foix", 9700);
+		Ville v06 = new Ville(6, "Pau", 77200);
+		Ville v07 = new Ville(7, "Marseille", 850700);
+		Ville v08 = new Ville(8, "Tarbes", 40600);
 		Collections.addAll(villes, v01, v02, v03, v04, v05, v06, v07, v08);
-
 		return villes;
 	}
 
-	@PostMapping
-	public ResponseEntity<String> insertPet(@RequestBody Ville nvVille) {
+	@GetMapping
+	public ArrayList<Ville> trouverVilles() {
+		return villes;
+	}
+
+	@GetMapping("/{id}")
+	public Ville trouverVille(@PathVariable int id) {
+		Ville ville = null;
 		for (Ville item : villes) {
-			if (item.getNom().equals(nvVille.getNom())) {
-				return ResponseEntity.badRequest().body("Il y a déjà une ville avec ce nom !");
+			if (item.getId() == id) {
+				ville = item;
+			}
+		}
+		return ville;
+	}
+
+	@PostMapping
+	public ResponseEntity<String> ajouterVille(@RequestBody Ville nvVille) {
+		for (Ville item : villes) {
+			if (item.getId() == (nvVille.getId())) {
+				return ResponseEntity.badRequest().body("Il y a déjà une ville avec cet ID !");
 			}
 
 		}
 		villes.add(nvVille);
 		return ResponseEntity.ok("Ville ajoutée");
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<String> modifierVille(@PathVariable int id, @RequestBody Ville ville) {
+		Ville villeATrouver = trouverVille(id);
+		villeATrouver.setNom(ville.getNom());
+		villeATrouver.setNbHabitants(ville.getNbHabitants());
+		return ResponseEntity.ok("Ville modifiée");
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> supprimerVille(@PathVariable int id) {
+		Ville ville = trouverVille(id);
+		if (ville != null) {
+			for (int i = 0; i < villes.size(); i++) {
+				if (villes.get(i).getId() == (ville.getId())) {
+					villes.remove(i);
+					return ResponseEntity.ok("Ville modifiée");
+				}
+			}
+		}
+		return ResponseEntity.badRequest().body("Cette ville n'existe pas");
 	}
 
 }
--- src/main/java/fr/diginamic/hello/entities/Ville.java
+++ src/main/java/fr/diginamic/hello/entities/Ville.java
@@ -1,47 +1,76 @@
 package fr.diginamic.hello.entities;
 
 public class Ville {
+
+	protected int id;
 	protected String nom;
 	protected int nbHabitants;
-	
-	/** Constructor
+
+	/**
+	 * Constructor
+	 * 
+	 * @param id
 	 * @param nom
 	 * @param nbHabitants
 	 */
-	public Ville(String nom, int nbHabitants) {
+	public Ville(int id,String nom, int nbHabitants) {
+		this.id= id;
 		this.nom = nom;
 		this.nbHabitants = nbHabitants;
 	}
 
-	/** Getter pour nom
+	/**
+	 * Getter pour nom
+	 * 
 	 * @return nom
 	 */
 	public String getNom() {
 		return nom;
 	}
 
-	/**Setter pour nom
-	 * @param nom nom 
+	/**
+	 * Setter pour nom
+	 * 
+	 * @param nom nom
 	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
-	/** Getter pour nbHabitants
+	/**
+	 * Getter pour nbHabitants
+	 * 
 	 * @return nbHabitants
 	 */
 	public int getNbHabitants() {
 		return nbHabitants;
 	}
 
-	/**Setter pour nbHabitants
-	 * @param nbHabitants nbHabitants 
+	/**
+	 * Setter pour nbHabitants
+	 * 
+	 * @param nbHabitants nbHabitants
 	 */
 	public void setNbHabitants(int nbHabitants) {
 		this.nbHabitants = nbHabitants;
 	}
-	
-	
-	
+
+	/**
+	 * Getter pour id
+	 * 
+	 * @return id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * Setter pour id
+	 * 
+	 * @param id id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
 
 }
